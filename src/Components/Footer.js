5e906d806fe9f4f8b098caf4df49a676d6829127
amirhosein <amirhosein.najafy@gmail.com>
2020-10-23 03:04:12 +0330

    creating footer by amirhosein

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,73 +1,69 @@
 import React from 'react';
 import './Footer.css';
 
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 function Footer() {
- //   <i class='fab fa-typo3' />
-  return (
-    <div className='footer-container'>
-   
-     
-        <div class='social-media-wrap'>
+    //   <i class='fab fa-typo3' />
+    return (
+        <div className='footer-container'>
 
 
-          <div class='footer-logo'>
-            <Link to='/' className='social-logo'>
-             تمام حقوق مالی و معنوی این سایت متعلق به جایی ؟ است
-           
-            </Link>
-          </div>
+            <div class='social-media-wrap'>
 
 
-          <small class='website-rights'>  © 2020</small>
+                <div class='footer-logo'>
+                    <p className='social-logo'>
+                        تمام حقوق مالی و معنوی این سایت متعلق به کجایی است ©
+
+                    </p>
+                </div>
+                <center>
+                    <div class='social-icons'>
+                        <Link
+                            class='social-icon-link twitter'
+                            to='/'
+                            target='_blank'
+                            aria-label='twitter'
+                        >
+                            <i class='fab fa-twitter'/>
+
+                        </Link>
+                        <Link
+                            class='social-icon-link facebook'
+                            to='/'
+                            target='_blank'
+                            aria-label='facebook'
+                        >
+                            <i class='fab fa-facebook'/>
+
+                        </Link>
+                        <Link
+                            class='social-icon-link telegram'
+                            to='/'
+                            target='_blank'
+                            aria-label='telegram'
+                        >
+                            <i class='fab fa-telegram'/>
+
+                        </Link>
+
+                        <Link
+                            class='social-icon-link instagram'
+                            to='/'
+                            target='_blank'
+                            aria-label='Instagram'
+                        >
+                            <i class='fab fa-instagram'/>
+                        </Link>
 
 
-          <div class='social-icons'>
-            <Link
-              class='social-icon-link telegram'
-              to='/'
-              target='_blank'
-              aria-label='telegram'
-            >
-              <i class='fab fa-telegram' />
-              
-            </Link>
-            <Link
-              class='social-icon-link telegram'
-              to='/'
-              target='_blank'
-              aria-label='telegram'
-            >
-              <i class='fab fa-telegram' />
-              
-            </Link>
-            <Link
-              class='social-icon-link telegram'
-              to='/'
-              target='_blank'
-              aria-label='telegram'
-            >
-              <i class='fab fa-telegram' />
-              
-            </Link>
+                    </div>
+                </center>
+            </div>
 
-            <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i class='fab fa-instagram' />
-            </Link>
-
-
-           
-          </div>
         </div>
-      
-    </div>
-  );
+    );
 }
 
 export default Footer;
