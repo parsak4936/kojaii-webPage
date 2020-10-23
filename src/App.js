7570cea467a0c109021d83d97b669a0a62da4d0f
amirhosein <amirhosein.najafy@gmail.com>
2020-10-23 13:56:36 +0330

    fixing header

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
-import React from 'react';
-import NavBar from './Components/nav_bar'
+import React, {Component} from 'react';
+import {Navbar,Nav,NavDropdown,Button} from 'react-bootstrap'
 import './App.css';
 import {BrowserRouter as Router , Switch ,Route} from 'react-router-dom'
 import Footer from './Components/Footer'
 import About from './Components/About';
- 
+
 import Download from './Components/Download';
 import MainPage from './Components/MainPage';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LOGO from "./Components/LOGO.png";
+import Avatar from "react-avatar";
+
+import logo from './LOGO.png'
+
 function App() {
-  return (
-   <>
+    return (
+        <>
 
-  <Router>
-  
-  <NavBar/>
-    <Switch>
-          <Route path='/' exact component=  {MainPage} />
-          <Route path='/about' component=   {About}    />
-         
-          <Route path='/download' component={Download} />
-    </Switch>
-  <Footer />
-   </Router>
-  
-  
+            <Router>
+
+                <Navbar className="nav_bar" collapseOnSelect expand="lg">
+                    <Navbar.Brand href="#home">
+                        <img src={logo} alt="Logo" width="160px"/>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" className="navbar-Item">
+                        <Nav className="mr-auto">
+                            <Nav.Link className="item" href="#features">خانه</Nav.Link>
+                            <Nav.Link className="item" href="#pricing">پشتیبانی</Nav.Link>
+
+                            <Nav.Link className="item" href="#deets">ارتباط با ما</Nav.Link>
+                            <Nav.Link className="item" href="#deets">بلاگ</Nav.Link>
+                        </Nav>
+
+                        <Nav>
+
+                            <Nav.Link href="#download">
+                                <Button variant="outline-light">دانلود <i className='fa fa-download'/></Button>{' '}
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
 
 
-  
-    </>
-  );
+                <Switch>
+                    <Route path='/' exact component=  {MainPage} />
+                    <Route path='/about' component=   {About}    />
+
+                    <Route path='/download' component={Download} />
+                </Switch>
+                <Footer />
+            </Router>
+
+
+
+
+
+        </>
+    );
 }
 
 export default App;
