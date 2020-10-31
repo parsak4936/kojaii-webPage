23e6e641f0ca67b05e1b3bc70ab93cba44159fce
amirhosein <amirhosein.najafy@gmail.com>
2020-10-31 14:37:34 +0330

    create introduction of kojaii

--- src/Components/nav_bar.js
+++ src/Components/nav_bar.js
@@ -1,10 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar,Nav,NavDropdown,Button} from 'react-bootstrap'
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import LOGO from './LOGO.png'
 import './nav_bar.css';
 import logo from "../LOGO.png";
+import $ from "jquery";
 
 function Nav_bar() {
 
@@ -19,39 +18,107 @@ function Nav_bar() {
         }
     };
 
+    let state = false;
+    const classChange = () => {
+        state = !state;
+        if (!state){
+            $('#myNavbar').addClass('collapse');
+            $('#toggle').removeClass('change');
+        }else{
+            $('#myNavbar').removeClass('collapse');
+            $('#toggle').addClass('change');
+        }
+    }
+
+
     useEffect(() => {
         showButton();
     }, []);
     //<i class='fab fa-typo3' />
     window.addEventListener('resize', showButton);
 
+    $(window).scroll(function() {
 
-  return (
+        let position    =   $(this).scrollTop();
+        if (position >= 100) {
+            $('.nav-menu').addClass('costum-navbar');
+
+        } else {
+            $('.nav-menu').removeClass('costum-navbar');
+        }
+
+    });
+
+
+// // ===== HAMBURGER BUTTON ===== //
+//     $(document).ready(function() {
+//         $('#toggle').on('click', function(){
+//             $('#myNavbar').toggleClass('collapse');
+//         })
+//     });
+
+    return (
     <>
-        <Navbar className="nav_bar" collapseOnSelect expand="lg">
-            <Navbar.Brand href="#home">
-                <img src={logo} alt="Logo" width="160px"/>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav" className="navbar-Item ">
-                <Nav className="mr-auto">
-                    <Nav.Link className="item" href="/">خانه</Nav.Link>
-                    <Nav.Link className="item" href="/">پشتیبانی</Nav.Link>
+        <nav className="navbar navbar-default navbar-expand-lg nav-menu" id="nav-menu">
 
-                    <Nav.Link className="item" href="/about">ارتباط با ما</Nav.Link>
-                    <Nav.Link className="item" href="/blog">بلاگ</Nav.Link>
-                </Nav>
+            <div className="container-fluid">
 
-                <Nav>
+                <a href="#" className="navbar-brand text-uppercase"> navbar </a>
 
-                    <Nav.Link href="#download">
-                        <Button className="download" variant="outline-light">دانلود <i className='fa fa-download'/></Button>{' '}
-                    </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+                <button onClick={classChange}  id="toggle" class="navbar-toggler" type="button" data-toggle="collapse"
+                                              data-target="#myNavbar" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <div className="line1 ham"></div>
+                    <div className="line2 ham"></div>
+                    <div className="line3 ham"></div>
+                </button>
+
+                <div className='collapse navbar-collapse justify-content-end'
+                     id="myNavbar">
+
+                    <ul className="navbar-nav ml-aulo" >
+
+                        <li className="nav-item">
+                            <a className="nav-link m-2 menu-item nav-active active" href="#"> Home </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link m-2 menu-item" href="#"> About </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link m-2 menu-item" href="#"> Services </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link m-2 menu-item" href="#"> Contact </a>
+                        </li>
 
 
+                    </ul>
+
+                </div>
+
+            </div>
+
+
+        </nav>
+
+        <section className="hero-section">
+
+            <div className="container-fluid">
+
+                <div className="row justify-content-center">
+
+                    <div className="col-12">
+                        <h1 className="text-hero text-center text-white text-uppercase"> navbar </h1>
+                    </div>
+
+                    <div className="col-12">
+                        <p className="lead text-center text-uppercase sub-text-hero"> scroll animation </p>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
     </>
   );
 }
