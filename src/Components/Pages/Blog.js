9c054cf191fa57b9f5e352f7bdbf5acb503e8d4c
parsak4936 <parsawriter1999@gmail.com>
2020-11-07 20:47:36 +0330

    fixed overflow in main page / fixed blog bugs

--- src/Components/Pages/Blog.js
+++ src/Components/Pages/Blog.js
@@ -13,7 +13,7 @@ function Blog() {
             <div className="blog-container">
                 <h3 className='blog-Header'>تیم نیگما در حال اماده ای ن صفحه یباشد ، از شکیبای شما سپاس گذاریم</h3>
                 <section className='blog-icon'>
-                    <TransverseLoading size = "large"  />;
+                    <TransverseLoading size = "large" style={{position:'relative', display:'flex' }}  />;
 
                 </section>
 
