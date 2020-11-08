23996f9cb4e63ebef4a75691c6c8a8759bd2f753
parsak4936 <parsawriter1999@gmail.com>
2020-11-08 12:27:22 +0330

    added countdown to master

--- src/Components/Pages/Blog.js
+++ src/Components/Pages/Blog.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Blog.css'
 
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 import { TransverseLoading } from 'react-loadingg';
 
 
+const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+        return <div className="timer">Too lale...</div>;
+    }
+
+    return (
+        <div className="timer">
+            <div className="text">Remaining</div>
+            <div className="value">{remainingTime}</div>
+            <div className="text">seconds</div>
+        </div>
+    );
+};
 
 function Blog() {
 
@@ -13,8 +27,14 @@ function Blog() {
             <div className="blog-container">
                 <h3 className='blog-Header'>تیم نیگما در حال اماده سازی این صفحه یباشد ، از شکیبای شما سپاس گذاریم</h3>
                 <section className='blog-icon'>
-                    <TransverseLoading size = "large" style={{position:'relative', display:'flex' }}  />;
-
+                    <CountdownCircleTimer
+                        isPlaying
+                        duration={10}
+                        colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+                        onComplete={() => [true, 1000]}
+                    >
+                        {renderTime}
+                    </CountdownCircleTimer>
                 </section>
 
 
