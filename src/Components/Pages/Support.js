1511814b7930b5d3460086bf19aa2ba166733e22
parsak4936 <parsawriter1999@gmail.com>
2020-11-09 13:18:56 +0330

    changes in support grid and components,image add in desktop mode in sup age

--- src/Components/Pages/Support.js
+++ src/Components/Pages/Support.js
@@ -4,7 +4,7 @@ import {Tabs, Tab, Row, Col, Nav, Form} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from "react-bootstrap/Button";
 import AbsoluteWrapper from "../AbsoluteWrapper";
-
+import  sup_img from '../Images/support.jpg'
 
 function Support() {
 
@@ -12,30 +12,30 @@ function Support() {
         <>
         <AbsoluteWrapper>
 <div className="sup-container">
-
+    <img src={sup_img} alt= " " width="400px" className="sup-img"/>
     <div className="tab-wrapper">
         <div className='container-fluid' >
+
+
             <div className="row">
 
                 <div className="col-sm-12">
 
-                    <Tab.Container defaultActiveKey="profile">
+                    <Tab.Container defaultActiveKey="report">
                         <Row>
                             <Col sm={3}>
 
                                 <Nav variant="pills" className="flex-column">
 
                                     <Nav.Item>
-                                        <Nav.Link eventKey="home">گزارش خطا در برنامه </Nav.Link>
+                                        <Nav.Link eventKey="report">گزارش خطا در برنامه </Nav.Link>
                                     </Nav.Item>
 
                                     <Nav.Item>
-                                        <Nav.Link eventKey="profile">پیشنهادات و انتقادات</Nav.Link>
+                                        <Nav.Link eventKey="suggest">پیشنهادات و انتقادات</Nav.Link>
                                     </Nav.Item>
 
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="contact"> </Nav.Link>
-                                    </Nav.Item>
+
 
                                 </Nav>
 
@@ -43,105 +43,103 @@ function Support() {
                             <Col sm={9}>
                                 <Tab.Content>
 
-                                    <Tab.Pane className="parag" eventKey="home">
+                                    <Tab.Pane className="parag" eventKey="report">
                                         <Form>
                                             <Form.Row>
                                                 <Form.Group as={Col} controlId="formGridEmail">
-                                                    <Form.Label>Email</Form.Label>
-                                                    <Form.Control type="email" placeholder="Enter email" />
+                                                    <Form.Label>نام</Form.Label>
+                                                    <Form.Control type="email" placeholder="نام خود را وارد کنید" />
                                                 </Form.Group>
 
+                                                <Form.Group as={Col} controlId="formGridEmail">
+                                                    <Form.Label>نام</Form.Label>
+                                                    <Form.Control type="email" placeholder="نام خانوادگی  خود را وارد کنید" />
+                                                </Form.Group>
 
                                             </Form.Row>
 
                                             <Form.Group controlId="formGridAddress1">
-                                                <Form.Label>Address</Form.Label>
-                                                <Form.Control placeholder="1234 Main St" />
+                                                <Form.Label>ایمیل</Form.Label>
+                                                <Form.Control placeholder="*******@gmail.com" />
                                             </Form.Group>
 
                                             <Form.Group controlId="formGridAddress2">
-                                                <Form.Label>Address 2</Form.Label>
-                                                <Form.Control placeholder="Apartment, studio, or floor" />
+                                                <Form.Label>تلفن همراه</Form.Label>
+                                                <Form.Control placeholder="02833669553" />
                                             </Form.Group>
 
                                             <Form.Row>
-                                                <Form.Group as={Col} controlId="formGridCity">
-                                                    <Form.Label>City</Form.Label>
-                                                    <Form.Control />
+                                                <Form.Group  as={Col} >
+
+                                                    <Form.Control disabled />
                                                 </Form.Group>
 
-                                                <Form.Group as={Col} controlId="formGridState">
-                                                    <Form.Label>State</Form.Label>
-                                                    <Form.Control as="select" defaultValue="Choose...">
-                                                        <option>Choose...</option>
-                                                        <option>...</option>
+                                                <Form.Group as={Col} >
+
+                                                    <Form.Control disabled>
                                                     </Form.Control>
                                                 </Form.Group>
 
-                                                <Form.Group as={Col} controlId="formGridZip">
-                                                    <Form.Label>Zip</Form.Label>
-                                                    <Form.Control />
+                                                <Form.Group as={Col}  disabled >
+                                                    <Form.Control disabled />
                                                 </Form.Group>
                                             </Form.Row>
 
-                                            <Form.Group id="formGridCheckbox">
-                                                <Form.Check type="checkbox" label="Check me out" />
-                                            </Form.Group>
 
-                                            <Button variant="primary" type="submit">
-                                                Submit
+
+                                            <Button  className="btn-accept"  variant="primary" type="submit">
+                                                تایید و ارسال
                                             </Button>
                                         </Form>
 
                                     </Tab.Pane>
 
-                                    <Tab.Pane className="parag" eventKey="profile">
+                                    <Tab.Pane className="parag" eventKey="suggest">
                                         <Form>
                                             <Form.Row>
                                                 <Form.Group as={Col} controlId="formGridEmail">
-                                                    <Form.Label>Email</Form.Label>
-                                                    <Form.Control type="email" placeholder="Enter email" />
+                                                    <Form.Label>نام</Form.Label>
+                                                    <Form.Control type="email" placeholder="نام خود را وارد کنید" />
                                                 </Form.Group>
 
+                                                <Form.Group as={Col} controlId="formGridEmail">
+                                                    <Form.Label>نام</Form.Label>
+                                                    <Form.Control type="email" placeholder="نام خانوادگی  خود را وارد کنید" />
+                                                </Form.Group>
 
                                             </Form.Row>
 
                                             <Form.Group controlId="formGridAddress1">
-                                                <Form.Label>Address</Form.Label>
-                                                <Form.Control placeholder="1234 Main St" />
+                                                <Form.Label>ایمیل</Form.Label>
+                                                <Form.Control placeholder="*******@gmail.com" />
                                             </Form.Group>
 
                                             <Form.Group controlId="formGridAddress2">
-                                                <Form.Label>Address 2</Form.Label>
-                                                <Form.Control placeholder="Apartment, studio, or floor" />
+                                                <Form.Label>تلفن همراه</Form.Label>
+                                                <Form.Control placeholder="02833669553" />
                                             </Form.Group>
 
                                             <Form.Row>
-                                                <Form.Group as={Col} controlId="formGridCity">
-                                                    <Form.Label>City</Form.Label>
-                                                    <Form.Control />
+                                                <Form.Group  as={Col} >
+
+                                                    <Form.Control disabled />
                                                 </Form.Group>
 
-                                                <Form.Group as={Col} controlId="formGridState">
-                                                    <Form.Label>State</Form.Label>
-                                                    <Form.Control as="select" defaultValue="Choose...">
-                                                        <option>Choose...</option>
-                                                        <option>...</option>
+                                                <Form.Group as={Col} >
+
+                                                    <Form.Control disabled>
                                                     </Form.Control>
                                                 </Form.Group>
 
-                                                <Form.Group as={Col} controlId="formGridZip">
-                                                    <Form.Label>Zip</Form.Label>
-                                                    <Form.Control />
+                                                <Form.Group as={Col}  disabled >
+                                                    <Form.Control disabled />
                                                 </Form.Group>
                                             </Form.Row>
 
-                                            <Form.Group id="formGridCheckbox">
-                                                <Form.Check type="checkbox" label="Check me out" />
-                                            </Form.Group>
 
-                                            <Button variant="primary" type="submit">
-                                                Submit
+
+                                            <Button  className="btn-accept"  variant="primary" type="submit">
+                                                تایید و ارسال
                                             </Button>
                                         </Form>
 
