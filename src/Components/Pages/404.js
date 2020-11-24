ea6ce21e3a4658e6b85e491cb75e79ec64a9b285
parsak4936 <parsawriter1999@gmail.com>
2020-11-24 13:02:08 +0330

    changes in 404 page

--- src/Components/Pages/404.js
+++ src/Components/Pages/404.js
@@ -10,35 +10,20 @@ export default class Support extends React.Component {
     render() {
         return (
             <>
-                <body class="bg-purple">
-                <div class="stars">
-
-                    <div class="central-body">
-                        <img class="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px"/>
-                            <Link to="/home" class="btn-go-home">بازگشت به خانه ی اصلی</Link>
+                <body>
+                <section id="not-found">
+                    <div id="title">Simple Pure CSS3 &bull; 404 Error Page</div>
+                    <div className="circles">
+                        <p>404<br></br>
+                            <small>PAGE NOT FOUND</small>
+                        </p>
+                        <span className="circle big"> </span>
+                        <span className="circle med"> </span>
+                        <span className="circle small"> </span>
                     </div>
-                    <div class="objects">
-                        <img class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px"/>
-                            <div class="earth-moon">
-                                <img class="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px"/>
-                                    <img class="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px" />
-                            </div>
-                            <div class="box_astronaut">
-                                <img class="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px"/>
-                            </div>
-                    </div>
-                    <div class="glowing_stars">
-                        <div class="star"></div>
-                        <div class="star"></div>
-                        <div class="star"></div>
-                        <div class="star"></div>
-                        <div class="star"></div>
-
-                    </div>
-
-                </div>
-
+                </section>
                 </body>
+
             </>
         )
     }
