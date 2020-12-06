19f1f59a1c4c565469113c6882fe08d7ba65b9e3
amirhosein <amirhosein.najafy@gmail.com>
2020-12-06 06:17:00 +0330

    connecting support form to api by amirhosein

--- src/Components/Pages/Support.js
+++ src/Components/Pages/Support.js
@@ -1,13 +1,9 @@
-// import React from 'react';
 import './Support.css'
-// import {Tab, Row, Col, Nav, Form} from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Button from "react-bootstrap/Button";
 import sup_img from '../Images/Support.svg'
 import bug_img from '../Images/bugrepo.png'
 import $ from "jquery";
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
@@ -18,7 +14,7 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
+    const {children, value, index, ...other} = props;
 
     return (
         <div
@@ -61,13 +57,74 @@ const useStyles = makeStyles((theme) => ({
     direction: 'rtl',
 }));
 
+let first_name;
+
+function handleSubmit_report() {
+    // var first_name = $(".first_name");
+    // var last_name = $(".last_name").val();
+    // var email = $(".email").val();
+    // var field_text = $(".field_text").val();
+    // var type = $("#type").val();
+
+    console.log(first_name);
+    // console.log(last_name);
+    // console.log(email);
+
+
+}
+
 export default function CenteredTabs() {
+
     const classes = useStyles();
     const [value, setValue] = React.useState(0);
+
+    let kind;
+
+    const [firstname, setfirstName] = React.useState("");
+    const [lastname, setlastName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [text, setText] = React.useState("");
+
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (firstname == "" || lastname == "" || email == "" || text == ""){
+            $('#error').css('display', 'block');
+            $("#error").html('لطفا تمام فیلد ستاره دار را تکمیل فرمایید');
+        }else if(!email.match(re)){
+        }else {
+            $.ajax({
+                url: 'https://kojaii.herokuapp.com/api/supports',
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    first_name: firstname,
+                    last_name: lastname,
+                    email: email,
+                    text: text,
+                    type: parseInt(value)
+                },
+                async: false,
+                complete: function (r) {
+                    if (r.status === 200) {
+                        $('#error').css('display', 'block');
+                        $('#error').css('color', 'green');
+                        $("#error").html('پیام شما با موفقیت ثبت شد با تشکر از شما');
+                    } else {
+                        alert(JSON.parse(r.responseText)['message']);
+                    }
+                }
+            });
+        }
+
+
+    }
 
     return (
         <>
@@ -81,72 +138,107 @@ export default function CenteredTabs() {
                             textColor="primary"
                             centered
                         >
-                            <Tab style={{outline:"none"}} label="گزارش خطا برنامه" {...a11yProps(0)} />
-                            <Tab style={{outline:"none"}} label="انتقادات و پیشنهادات" {...a11yProps(1)} />
+                            <Tab style={{outline: "none"}} label="گزارش خطا برنامه" {...a11yProps(0)} />
+                            <Tab style={{outline: "none"}} label="انتقادات و پیشنهادات" {...a11yProps(1)} />
                         </Tabs>
-                        <TabPanel  value={value} index={0}>
-                            <img className="images" src={bug_img} style={{float :"left"}}/>
+                        <TabPanel value={value} index={0}>
+                            <img className="images" src={bug_img} style={{float: "left"}}/>
                             <form dir="rtl" className={classes.formStyle} noValidate autoComplete="off">
 
-                                <TextField dir="rtl" className="names" id="standard-basic" label="نام" placeholder="" required/>
-                                <TextField className="names" id="standard-basic" label="نام خانوادگی" required/>
+                                <TextField
+                                    className="names first_name"
+                                    id="standard-basic"
+                                    label="نام"
+                                    value={firstname}
+                                    onChange={e => setfirstName(e.target.value)}
+                                    required
+                                />
+                                <TextField
+                                    className="names last_name"
+                                    id="standard-basic"
+                                    label="نام خانوادگی"
+                                    value={lastname}
+                                    onChange={e => setlastName(e.target.value)}
+                                    required/>
                                 <br/>
                                 <TextField
-                                    className="fields"
+                                    className="fields email"
                                     type="email"
                                     id="standard-basic"
                                     label="ایمیل"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
                                     required
                                 />
                                 <br/>
                                 <TextField
-                                    className="fields"
+                                    className="fields field_text"
                                     label="گزارش خطا"
                                     multiline
                                     rows={2}
                                     rowsMax={4}
+                                    value={text}
+                                    onChange={e => setText(e.target.value)}
                                     required
                                 />
+                                <input id="kind_value" type="hidden" value="1"/>
                             </form>
-                            <div className="submit-btn">
-                                <Button type="submit" variant="contained" color="primary">
-                                    تایید و ارسال
-                                </Button>
-                            </div>
-                        </TabPanel>
-                        <TabPanel value={value} index={1}>
-                            <img className="images" src={sup_img} style={{float :"left"}}/>
-                            <form dir="rtl" className={classes.formStyle} noValidate autoComplete="off">
-
-                                <TextField dir="rtl" className="names" id="standard-basic" label="نام" placeholder="" required />
-
-                                <TextField className="names" id="standard-basic" label="نام خانوادگی" required />
-                                <br/>
-                                <TextField
-                                    className="fields"
-                                    type="email"
-                                    id="standard-basic"
-                                    label="ایمیل"
-                                    required
-                                />
-                                <br/>
-                                <TextField
-                                    className="fields"
-                                    label="گزارش خطا"
-                                    multiline
-                                    rows={2}
-                                    rowsMax={4}
-                                    required
-                                />
-                            </form>
-                            <Button type="submit" variant="contained" color="primary">
+                            <br/>
+                            <span style={{color:"red",display:"none"}} id="error"></span>
+                            <br/>
+                            <Button onClick={handleSubmit} type="submit" variant="contained" color="primary">
                                 تایید و ارسال
                             </Button>
-                            <div className="submit-btn">
-                                <Button type="submit" variant="contained" color="primary">
-                                    تایید و ارسال
-                                </Button>
-                            </div>
+
+                        </TabPanel>
+                        <TabPanel value={value} index={1}>
+                            <img className="images" src={sup_img} style={{float: "left"}}/>
+                            <form dir="rtl" className={classes.formStyle} noValidate autoComplete="off">
+
+                                <TextField
+                                    className="names first_name"
+                                    id="standard-basic"
+                                    label="نام"
+                                    value={firstname}
+                                    onChange={e => setfirstName(e.target.value)}
+                                    required
+                                />
+                                <TextField
+                                    className="names last_name"
+                                    id="standard-basic"
+                                    label="نام خانوادگی"
+                                    value={lastname}
+                                    onChange={e => setlastName(e.target.value)}
+                                    required/>
+                                <br/>
+                                <TextField
+                                    className="fields email"
+                                    type="email"
+                                    id="standard-basic"
+                                    label="ایمیل"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    required
+                                />
+                                <br/>
+                                <TextField
+                                    className="fields field_text"
+                                    label="گزارش خطا"
+                                    multiline
+                                    rows={2}
+                                    rowsMax={4}
+                                    value={text}
+                                    onChange={e => setText(e.target.value)}
+                                    required
+                                />
+                                <input id="kind_value" type="hidden" value="1"/>
+                            </form>
+                            <br/>
+                            <span style={{color:"red",display:"none"}} id="error"></span>
+                            <br/>
+                            <Button onClick={handleSubmit} type="submit" variant="contained" color="primary">
+                                تایید و ارسال
+                            </Button>
                         </TabPanel>
                     </Paper>
                 </div>
@@ -167,8 +259,6 @@ export default function CenteredTabs() {
 //         this.handleSubmit_report = this.handleSubmit_report.bind(this);
 //         this.handleSubmit_suggest = this.handleSubmit_suggest.bind(this);
 //     }
-//
-// //TODO 4) slideshow ? (should check later)
 //
 //     handleChange_report = (event) => {
 //         this.setState({
